18bdc27b6c41a027957d878af33ab29ff8db2b8a
“Clarkey311” <jclarke89@outlook.com>
2020-04-22 11:47:17 +0100

    Created a basic searchField using state.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { robots } from './robots';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js';
 
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
 
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value});
+        //console.log(event.target.value);
+        
+        //console.log(filteredRobots);
 
-const App = () =>{
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    )
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots}/>
+            </div>
+        )
+    }
 }
 
 export default App;
