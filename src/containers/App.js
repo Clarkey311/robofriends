423754ae8e2c1da4f1ebd1110e8b32afcafc054e
“Clarkey311” <jclarke89@outlook.com>
2020-04-22 16:43:11 +0100

    Added ErrorBountry function

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from '../components/CardList.js';
 import SearchBox from '../components/SearchBox.js';
 import './App.css';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component {
     constructor() {
@@ -40,9 +41,11 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <CardList robots={filteredRobots}/>
-                    </Scroll>
+                    <ErrorBoundry>
+                        <Scroll>
+                            <CardList robots={filteredRobots}/>
+                        </Scroll>
+                    </ErrorBoundry>
                 </div>
             )
         }
